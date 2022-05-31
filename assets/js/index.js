e567958ad4f4da2f44088021edd718773633d573
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-31 10:18:21 +0300

    Example getters for element

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function logMessage() {
+  alert("message");
+}
+
+// const [button] = document.getElementsByTagName("button");
+// button.addEventListener("click", logMessage);
+
+// const id = 'unique'
+// const unique = document.getElementById(id);
+// unique.addEventListener("click", logMessage);
+
+// const unique = document.querySelector("#unique");
+// const btn1 = document.querySelector("button");
+
+// const btns1 = document.getElementsByClassName("btn");
+// const btns2 = document.querySelectorAll(".btn");
