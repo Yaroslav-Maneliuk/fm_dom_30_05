b4dc7e7f21088d22736fc643c8ab217d78fd8830
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-31 11:00:17 +0300

    Example select

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,5 @@
 "use strict";
 
-function logMessage() {
-  alert("message");
-}
-
-// const [button] = document.getElementsByTagName("button");
-// button.addEventListener("click", logMessage);
-
-// const id = 'unique'
-// const unique = document.getElementById(id);
-// unique.addEventListener("click", logMessage);
-
-// const unique = document.querySelector("#unique");
-// const btn1 = document.querySelector("button");
-
-// const btns1 = document.getElementsByClassName("btn");
-// const btns2 = document.querySelectorAll(".btn");
-
 const img1 = document.querySelector('img');
 
 const [img] = document.getElementsByTagName("img");
