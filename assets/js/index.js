30c7b8d38cb8a74638e6d22f1af6a1627c489bc6
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-31 10:59:59 +0300

    Example select element

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,13 @@ function logMessage() {
 
 // const btns1 = document.getElementsByClassName("btn");
 // const btns2 = document.querySelectorAll(".btn");
+
+const img1 = document.querySelector('img');
+
+const [img] = document.getElementsByTagName("img");
+
+const [, text] = document.getElementsByClassName("text");
+
+const li2 = document.querySelector('li:nth-child(2)');
+
+const ps = document.querySelectorAll('#main p')
