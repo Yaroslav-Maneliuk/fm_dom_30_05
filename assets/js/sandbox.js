59877e2a962264d061b6b39b739dc4c5ef69619a
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-01 10:36:37 +0300

    Example exercise

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,17 +1,39 @@
 "use strict";
 
-function createCounter1() {
-  let i = 0;
-  return {
-    increment() {
-      return ++i;
-    },
-    decrement(){
-      return --i;
-    },
-  };
-}
+// function createCounter1() {
+//   let i = 0;
+//   return {
+//     increment() {
+//       return ++i;
+//     },
+//     decrement(){
+//       return --i;
+//     },
+//   };
+// }
 
-const counter1 = createCounter1(10);
-console.log(counter.increment());
-console.log(counter.decrement());
+// const counter1 = createCounter1(10);
+// console.log(counter1.increment());
+// console.log(counter1.decrement());
+
+//---------------------------------------//
+// function createAdder(n) {
+//   // let sum = n;
+//   return function adder(m) {
+//     return (n += m);
+//   };
+// }
+
+//---------------------------------------//
+
+// function createAdder(n) {
+//   return (m) => (n += m);
+// }
+
+//---------------------------------------//
+
+const createAdder = ((n) => (m) => (n += m));
+
+const adder = createAdder(100);
+console.log(adder(15));
+console.log(adder(10));
