e998b451faf9d77b55240f4a374ee6e11cfc4eaa
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-03 11:49:52 +0300

    Before refactoring

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const cardsList = document.getElementById("cards-list");
-const HTMLCards = actors.map((actor) => createCard(actor));
+const HTMLCards = actors
+  .filter(({ name, photo }) => name || photo)
+  .map((actor) => createCard(actor));
 
 function createCard(actor) {
   const card = document.createElement("li");
@@ -10,23 +12,59 @@ function createCard(actor) {
   const container = document.createElement("article");
   container.classList.add("card-container");
 
+  const photoWrapper = document.createElement("div");
+  photoWrapper.classList.add("card-photo-wrapper");
+
+  const initials = document.createElement("div");
+  initials.classList.add("card-initials");
+  initials.style.backgroundColor = stringToColour(actor.name);
+  initials.append(
+    document.createTextNode(actor.name[0] || "NN")
+  ); /* homework */
+
   const photo = document.createElement("img");
   photo.classList.add("card-photo");
-  photo.setAttribute('src', actor.photo);
-  photo.setAttribute('alt', actor.name);
+  photo.setAttribute("src", actor.photo);
+  photo.setAttribute("alt", actor.name);
+  photo.addEventListener("error", photoErrorHandler);
+
+  photoWrapper.append(initials, photo);
 
   const fullName = document.createElement("h2");
   fullName.classList.add("card-fullName");
-  fullName.append(document.createTextNode(actor.name));
+  fullName.append(document.createTextNode(actor.name || "No name"));
 
   const description = document.createElement("p");
   description.classList.add("card-description");
-  description.append(document.createTextNode(actor.birthdate));
+  description.append(
+    document.createTextNode(actor.birthdate || "Month d,YYYY")
+  );
 
-  container.append(photo, fullName, description);
+  container.append(photoWrapper, fullName, description);
 
   card.append(container);
   return card;
 }
 
-// cardsList.append(...HTMLCards);
+cardsList.append(...HTMLCards);
+
+/* handler */
+
+function photoErrorHandler({ target }) {
+  target.remove();
+}
+
+/* utilits */
+
+function stringToColour(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = "#";
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).slice(-2);
+  }
+  return colour;
+}
