6085d4614dcd61d2e72949e589f8682b1164e675
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-06 11:30:03 +0300

    Add submit at form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
+"use strict";
 
+const [link] = document.links;
+link.addEventListener('click', (e)=>{
+  e.preventDefault();
+})
+
+const form = document.forms.rootForm;
+
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  // console.log(e)
+  console.log(e.target.elements.inputText)
+  console.dir(e.target.elements.inputText)
+})
