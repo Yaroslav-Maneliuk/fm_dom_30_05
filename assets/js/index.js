1cf0301d4cf422eaa8a3d261971e3640cc26f52d
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-07 18:09:43 +0300

    pattern

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,14 @@ const messages = [];
 const form = document.forms.rootForm;
 const list = document.getElementById('list');
 
+const pattern = /^([A-Z][a-z]+ [A-Z]\.)|([А-Я][а-я]+ [А-Я]\.)$/g
+
 form.addEventListener('submit', (e)=>{
   e.preventDefault();
   //при нажатии на кнопку Send добавлять значение из инпута в массив
   //в том случае если оно не пустое
   const inputValue = e.target.elements.inputText.value.trim();
-  if(inputValue){
+  if(pattern.test(inputValue)){
     messages.push(inputValue);
     //добавить автоматическую очистку формы
     e.target.reset();
--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -3,7 +3,7 @@
 // const pattern1 = /\b\w{4}\b/gi;
 // const pattern2 = new RegExp("\\bw{4}\\\b", "gi");
 
-const str = `qwe 1999god zxc 12USD qwerty`;
+const str = `qwe 1999god zxc 1288USD qwerty`;
 
 const pattern1 = /\b(\d{4})[a-z]+\b/g;
 const pattern2 = /\b(\d+)([A-Z]+)\b/g;
