c0883e94621a5f8fae4aa9e743487a27cfe46936
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-07 11:35:38 +0300

    Exzmple create RegX

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,20 @@
 "use strict";
 
-const [link] = document.links;
-link.addEventListener('click', (e)=>{
-  e.preventDefault();
-})
-
+const messages = [];
 const form = document.forms.rootForm;
+const list = document.getElementById('list');
 
 form.addEventListener('submit', (e)=>{
   e.preventDefault();
-  // console.log(e)
-  console.log(e.target.elements.inputText)
-  console.dir(e.target.elements.inputText)
+  //при нажатии на кнопку Send добавлять значение из инпута в массив
+  //в том случае если оно не пустое
+  const inputValue = e.target.elements.inputText.value.trim();
+  if(inputValue){
+    messages.push(inputValue);
+    //добавить автоматическую очистку формы
+    e.target.reset();
+  }
+  //HOMEWORK
+  //рендерить  в список все сообщения из массива
+  console.log(messages);
 })
---
+++ assets/js/sandbox.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const pattern1 = /\b\w{4}\b/gi;
+// const pattern2 = new RegExp("\\bw{4}\\\b", "gi");
+
+const str = `qwe 1999god zxc 12USD qwerty`;
+
+const pattern1 = /\b(\d{4})[a-z]+\b/g;
+const pattern2 = /\b(\d+)([A-Z]+)\b/g;
+if (pattern2.test(str)) {
+  // console.log(str.replace(pattern1, "$1 year"));
+  console.log(str.replace(pattern2, "$2 $1"));
+} else {
+  console.log(2);
+}
