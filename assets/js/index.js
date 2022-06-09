7ccd31e7ba4dc86fcf550a24a089fd2ae3e49c2c
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-09 09:58:33 +0300

    Add serialize & deserialize

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,53 @@
 "use strict";
 
-// const btn = document.getElementById("btn");
-
-// btn.addEventListener("click", () => {
-//   //while(true){}
-// });
-
-// console.log(1);
-// setTimeout(() => {
-//   console.log(3);
-// }, 1500);
-// console.log(2);
-
-// const identificator = setTimeout(() => {
-//   console.log("hi");
-// }, 200);
-// clearTimeout(identificator);
-
-const logNumberWithDelay = function () {
-  let count = 0;
-  const identificator = setInterval(() => {
-    console.log(count++);
-    if (count > 10) {
-      clearInterval(identificator);
-    }
-  }, 300);
+const user = {
+  name: "Elon",
+  surname: "Musk",
+  age: 50,
+  getfullName() {
+    return `${this.name} ${this.surname}`;
+  },
+  isMale: true,
+  pet: undefined,
+  isUkraine: null,
+  [Symbol("test")]: 123,
+  children: ["one", "two"],
+  cars: {
+    car1: { color: "red" },
+    car2: { color: "pink" },
+  },
 };
-console.log(logNumberWithDelay())
 
-// function a(){
-//   for(let i = 0; l<10; i++){
-//     console.log(a);
-//   }
-// }
+console.log(user);
+const serializeUser = JSON.stringify(user);
+console.log(serializeUser);
+
+
+const deserializeUser = JSON.parse(serializeUser);
+console.log(deserializeUser);
+
+
+const user2 = {
+  name: "Tim",
+  surname: "Cook",
+  age: 61,
+  getfullName() {
+    return `${this.name} ${this.surname}`;
+  },
+  isMale: true,
+  pet: undefined,
+  isUkraine: null,
+  [Symbol("test")]: 123,
+  brothers: {
+    brothers1: { brotherName: "Gerald Cook" },
+    brothers2: { brotherName: "Michael Cook" },
+  },
+};
+
+console.log(user2);
+const serializeUser2 = JSON.stringify(user2);
+console.log(serializeUser2);
+
+
+const deserializeUser2 = JSON.parse(serializeUser2);
+console.log(deserializeUser2);
